56e1e0e21bd86036d9f2264a5070e677ec58fa17
Firdavs Mukhiddinov <168963331+fedko11@users.noreply.github.com>
2024-05-06 12:22:55 +0500

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,42 @@
+
+#                                       ⁡⁢⁣⁢PRINT - HAQIDA TUSHUNCHA⁡
+
+#       Python dasturlash tilida ⁡⁢⁣⁢print()⁡ funksiyasi ⁡⁢⁣⁢ekranga⁡ ⁡⁢⁣⁢matn⁡ yoki ⁡⁢⁣⁢ma'lumot⁡ chiqarish uchun ishlatiladi
+
+#          ⁡⁣⁣     ⁡⁢⁢⁢EXAMPLE⁡
+#         print(" HELLO WORLD ")
+#    ⁡⁣⁢⁣Consolega chiqadi⁡ >>> ⁡⁢⁣⁢HEllO WORLD
+
+#                                        PRINT - YOZISH USULLARI
+
+
+#? print('Hello world')
+
+# print("Hello world")
+
+# print("o'rgimchak")
+
+# print('o\'rgimchak')
+
+# print("hello \"uzbekistan\"")
+
+# print('''Qayd etilishicha, Toshkent viloyati h
+# okimi Zoir Mirzayev korxona 
+# vakillari bilan onlayn 
+# muloqot olib borib, loyihani
+# amalga oshirishga qaratilgan memorandumni imzolagan''')
+
+# print("""hello "uzbekistan" """)
+
+# print("%.2f" % 3.1456985522)
+
+# print("salom")
+
+# print("\tsalom")
+
+# print("salom", "maktab", "hayr", "maktab", sep="\n")⁡
+
+
+
+
+
---
+++ SEP.py
@@ -0,0 +1,15 @@
+
+
+#!                                           SEP - HAQIDA TUSHUNCHA
+                    
+#                        ⁡⁣⁣⁢ Python dasturlash tilida ⁡⁢⁣⁢SEP ⁡⁣⁣⁢parameteri print() funktsiyasiga ⁡
+#                         ⁡⁣⁣⁢yuborilgan⁡ ⁡⁢⁣⁢argumentlarni ajratish uchun ⁡⁣⁣⁢ishlatiladi. Misol uchun:⁡
+
+#  ⁡⁢⁢  ⁢ ⁡⁢⁢⁢CODE⁡
+a = " Python "
+b = " dasturlash "
+d = " tili "
+print(a,b,d, sep='+')
+
+#    ⁡⁣⁢⁡⁢⁢⁢ IZOH  ⁡⁡                                               
+# ⁡⁢⁣⁣Consoleda chiqadi (orasidagi ⁡⁢⁣⁢" + "⁡ ⁡⁢⁣⁣larning hammasi⁡ ⁡⁢⁣⁢SEP⁡ ⁡⁢⁣⁣orqali qo'yilayapdi!⁡)⁡ >>> ( ⁡⁣⁢⁣Python ⁡⁢⁣⁢+ ⁡⁣⁢⁣dasturlash ⁡⁡⁢⁣⁢+⁡ ⁡⁣⁢⁡⁣⁢⁣tili ⁡)
